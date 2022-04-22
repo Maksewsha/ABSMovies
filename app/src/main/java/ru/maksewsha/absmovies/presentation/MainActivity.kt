9c3744bc90a1d7f3776884710691e0ff60b7b3a7
Sona <abc@mail.ru>
2022-04-22 16:07:28 +0300

    First logic

--- app/src/main/java/ru/maksewsha/absmovies/presentation/MainActivity.kt
+++ app/src/main/java/ru/maksewsha/absmovies/presentation/MainActivity.kt
@@ -1,12 +1,62 @@
 package ru.maksewsha.absmovies.presentation
 
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import ru.maksewsha.absmovies.R
+import ru.maksewsha.absmovies.databinding.ActivityMainBinding
+import ru.maksewsha.absmovies.presentation.fragments.RegistrationFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var bottomNav: BottomNavigationView
+    private var backCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        bottomNav = viewBinding.appBottomNav
+//        bottomNav.setOnItemSelectedListener {
+//            when(it.itemId){
+//                R.id.app_menu_btn_search -> {
+//                    if (supportFragmentManager.findFragmentById(R.id.fragment))
+//                }
+//                R.id.app_menu_btn_favorite -> {
+//
+//                }
+//                R.id.app_menu_btn_settings -> {
+//
+//                }
+//            }
+//        }
+
+        bottomNav.visibility = View.INVISIBLE
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, RegistrationFragment(), "RegistrationFragment")
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        Log.d("Activity", getFragment().id.toString())
+        when (getFragment().tag) {
+            "RegistrationFragment" -> {
+                backCount++
+                Toast.makeText(this, R.string.back_press, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getFragment(): Fragment {
+        val stackCount = supportFragmentManager.backStackEntryCount
+        return supportFragmentManager.fragments[if (stackCount > 0) stackCount - 1 else stackCount]
     }
 }
---
+++ app/src/main/java/ru/maksewsha/absmovies/presentation/fragments/RegistrationFragment.kt
@@ -0,0 +1,8 @@
+package ru.maksewsha.absmovies.presentation.fragments
+
+import androidx.fragment.app.Fragment
+import ru.maksewsha.absmovies.R
+
+class RegistrationFragment: Fragment(R.layout.fragment_reg) {
+    
+}
